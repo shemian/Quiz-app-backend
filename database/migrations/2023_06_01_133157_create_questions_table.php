d3c204a29785f966cf93571f925d687c95a50d7b
shemian <shemian092@gmail.com>
2023-06-12 07:05:20 -0700

    A\Create questions based on the education level and system

--- database/migrations/2023_06_01_133157_create_questions_table.php
+++ database/migrations/2023_06_01_133157_create_questions_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->foreignUuid('subject_id')->constrained()->cascadeOnDelete();
+            $table->uuid('education_system_id')->constrained('education_systems', 'id')->cascadeOnDelete();
+            $table->uuid('education_level_id')->constrained('education_levels', 'id')->cascadeOnDelete();
             $table->string('subtopic');
             $table->string('question');
             $table->string('option1');
