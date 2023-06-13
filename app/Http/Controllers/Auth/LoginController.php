2da50c8a595182ad8a36b2a677eb06fedac2ce1e
shemian <shemian092@gmail.com>
2023-06-13 10:42:27 -0700

    Enable to login with Pin

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
@@ -20,27 +21,23 @@ class LoginController extends Controller
 
     public function username()
     {
-        return 'email';
+        $login = request()->input('centy_plus_id'); // Get the input value from the login form
+
+        $fieldType = filter_var($login, FILTER_VALIDATE_EMAIL) ? 'email' : 'centy_plus_id'; // Determine if the input is an email or centy_plus_id
+
+        request()->merge([$fieldType => $login]); // Merge the input value into the request
+
+        return $fieldType; // Return the field type (email or centy_plus_id)
     }
 
     protected function attemptLogin(Request $request)
     {
         $credentials = $this->credentials($request);
 
-        // Check if the input is an email
-        if (filter_var($credentials[$this->username()], FILTER_VALIDATE_EMAIL)) {
-            // Attempt to authenticate using email and password
-            return $this->guard()->attempt(
-                $credentials,
-                $request->filled('remember')
-            );
-        } else {
-            // Attempt to authenticate using name and password
-            return $this->guard()->attempt(
-                ['name' => $credentials[$this->username()], 'password' => $credentials['password']],
-                $request->filled('remember')
-            );
-        }
+        return $this->guard()->attempt(
+            $credentials,
+            $request->filled('remember')
+        );
     }
 
     protected function sendFailedLoginResponse(Request $request)
@@ -52,7 +49,7 @@ class LoginController extends Controller
         }
 
         return redirect()->back()
-            ->withInput($request->only('email', 'remember'))
+            ->withInput($request->only('centy_plus_id', 'remember'))
             ->withErrors($errors);
     }
 
@@ -66,9 +63,8 @@ class LoginController extends Controller
             return redirect()->route('admin.dashboard');
         } elseif ($user->role === 'student') {
             return redirect()->route('student.dashboard');
-        }else {
+        } else {
             return redirect()->route('home');
         }
     }
-    
 }
