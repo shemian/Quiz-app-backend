b0e9b4d3088ba793f9d8002f046f060aeef60862
shemian <shemian092@gmail.com>
2023-06-13 09:32:02 -0700

    add phone_number and centy_plus_id

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,8 @@ class User extends Authenticatable
         'name',
         'role',
         'email',
+        'phone_number',
+        'centy_plus_id',
         'password',
     ];
 
@@ -53,5 +55,38 @@ class User extends Authenticatable
     public function guardian()
     {
         return $this->hasOne(Guardian::class);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        self::creating(function ($user) {
+            if ($user->role === 'parent') {
+                $user->centy_plus_id = 'CNT' . self::generateParentSequence($user->phone_number) . '-' . $user->phone_number;
+            } elseif ($user->role === 'student' || $user->role === 'teacher') {
+                $user->centy_plus_id = 'CNT-' . self::generateStudentTeacherSequence();
+            }
+        });
+    }
+
+    public static function generateParentSequence()
+    {
+        $parentSequence = static::where('role', 'parent')->count();
+        return $parentSequence + 1;
+    }
+
+    protected static function generateStudentTeacherSequence()
+    {
+        // Retrieve the last student/teacher
+        $lastUser = static::whereIn('role', ['student', 'teacher'])->latest('centy_plus_id')->first();
+
+        if ($lastUser) {
+            $sequence = intval(substr($lastUser->centy_plus_id, -7)) + 1; // Extract sequence and increment
+        } else {
+            $sequence = 1;
+        }
+
+        return str_pad($sequence, 7, '0', STR_PAD_LEFT);
     }
 }
