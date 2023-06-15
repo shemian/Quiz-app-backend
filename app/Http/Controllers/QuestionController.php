0924f3a000a684a1e70fe3265eefffe74f857232
shemian <shemian092@gmail.com>
2023-06-15 08:18:17 -0700

    education controller set up

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\EducationSystemLevelSubject;
+use App\Models\SubTopicSubStrand;
+use App\Models\TopicStrand;
 use Illuminate\Http\Request;
 
 use App\Models\Subject;
 use App\Models\Question;
 use App\Models\EducationSystem;
 use App\Models\EducationLevel;
+
+use Illuminate\Support\Facades\Log;
 
 class QuestionController extends Controller
 {
@@ -18,7 +22,7 @@ class QuestionController extends Controller
     public function index()
     {
         $subjects = Subject::all();
-        $education_systems = EducationSystem::all();
+        $education_systems = EducationSystem::with('educationLevels')->get();
         $education_level_system_subjects = EducationSystemLevelSubject::with('subject', 'educationSystem', 'educationLevel')->get();
         return view('teachers.questions', compact('subjects', 'education_level_system_subjects', 'education_systems'));
     }
@@ -52,6 +56,55 @@ class QuestionController extends Controller
         return redirect()->route('get_questions')->with('success', 'Question created successfully.');
 
     }
+
+    public function getEducationLevels(Request $request)
+    {
+        $educationSystemId = $request->input('educationSystemId');
+        $educationSystem = EducationSystem::with('educationLevels')->find($educationSystemId);
+        $educationLevels = $educationSystem->educationLevels;
+
+        Log::info($educationLevels);
+
+        return response()->json(['educationLevels' => $educationLevels]);
+    }
+
+
+    public function getSubjects(Request $request)
+    {
+        $educationSystemId = $request->input('educationSystemId');
+        $educationLevelId = $request->input('educationLevelId');
+
+        $subjects = EducationSystemLevelSubject::where('education_system_id', $educationSystemId)
+            ->where('education_level_id', $educationLevelId)
+            ->with('subject')
+            ->get()
+            ->pluck('subject');
+
+        return response()->json(['subjects' => $subjects]);
+    }
+
+
+    public function getTopics(Request $request)
+    {
+        $subjectId = $request->input('subjectId');
+        $subject = Subject::with('topicStrands')->find($subjectId);
+        $topicStrands = $subject->topicStrands;
+
+        return response()->json(['topics' => $topicStrands]);
+    }
+
+    public function getSubtopics(Request $request)
+    {
+        $topicId = $request->input('topicId');
+        $topicStrands = TopicStrand::with('subTopicSubStrands')->find($topicId);
+        $subTopicSubStrands = $topicStrands->subTopicSubStrands;
+
+        return response()->json(['subtopics' => $subTopicSubStrands]);
+    }
+
+
+
+
 
     /**
      * Display the specified resource.
