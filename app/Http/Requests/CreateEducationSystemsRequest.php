a0faf6cb5fdd39e6d556ec019a73f6c729208b8d
shemian <shemian092@gmail.com>
2023-06-20 01:42:48 -0700

    project update

--- app/Http/Requests/CreateEducationSystemsRequest.php
+++ app/Http/Requests/CreateEducationSystemsRequest.php
@@ -22,7 +22,7 @@ class CreateEducationSystemsRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required| unique:education_systems',
+            'name' => 'required',
         ];
     }
 }
