b4d27ab29e3376c0beda1d054a383a988797bd72
shemian <shemian092@gmail.com>
2023-06-21 08:45:27 -0700

    create the api routes to handle the mpesa transactions

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\MpesaTransactionController;
+use App\Models\MpesaTransaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::prefix('/v1')->group(function() {
+    Route::post('/access/token', [MpesaTransactionController::class, 'generateAccessToken']);
+    Route::post('/quiz/stk/push', [MpesaTransactionController::class, 'customerMpesaSTKPush']);
+    Route::post('/quiz/validation', [MpesaTransactionController::class, 'mpesaValidation']);
+    Route::post('/quiz/transaction/confirmation', [MpesaTransactionController::class, 'mpesaConfirmation']);
 });
