ad0711c0047b5bdec07e826e3e0f218a4d77c932
shemian <shemian092@gmail.com>
2023-06-21 08:44:12 -0700

    Included the eduaction_level in the subjects migrations

--- database/migrations/2023_05_13_132639_create_subjects_table.php
+++ database/migrations/2023_05_13_132639_create_subjects_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->string('name');
             $table->uuid('education_system_id')->constrained('education_systems', 'id')->cascadeOnDelete();
+            $table->uuid('education_level_id')->constrained('education_levels', 'id')->cascadeOnDelete();
             $table->timestamps();
         });
     }
