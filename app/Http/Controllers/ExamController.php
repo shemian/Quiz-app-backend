cc4eb945632d072f4d0b6a196e19a1df5e56eb30
shemian <shemian092@gmail.com>
2023-06-27 14:29:49 -0700

    Update Exam Controller

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\CreateExamRequest;
 use App\Models\EducationSystem;
 use App\Models\Exam;
 use App\Models\Question;
+use App\Models\Teacher;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -28,17 +29,35 @@ class ExamController extends Controller
     /**
      * Store a newly created resource in storage.
      */
+
     public function store(CreateExamRequest $request)
     {
         $data = $request->validated();
 
+        $user = auth()->user();
+
+        if (!$user) {
+            // Handle the case when the user is not authenticated
+            return redirect()->back()->with('error', 'You must be logged in to create a subject.');
+        }
+
+        $teacher = Teacher::where('user_id', $user->id)->first();
+
+        if (!$teacher) {
+            // Handle the case when the authenticated user is not a teacher
+            return redirect()->back()->with('error', 'You must be a teacher to create a subject.');
+        }
+
+
         $newExamRecord = new Exam();
+        $newExamRecord->teacher_id = $teacher->id;
         $newExamRecord->name = $data['name'];
         $newExamRecord->subject_id = $data['subject_id'];
         $newExamRecord->save();
 
         return redirect()->route('get_exams')->with('success', 'Exam Created successfully!');
     }
+
 
     /**
      * Display the specified resource.
