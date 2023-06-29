d2a24af69c0fcba06a1e2931fe6ef480e52777e0
shemian <shemian092@gmail.com>
2023-06-29 03:02:29 -0700

    Create CRUD routes for  the teacher

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -2,22 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\EducationSystem;
 use App\Models\Exam;
+use App\Models\Question;
+use App\Models\Subject;
+use App\Models\Teacher;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class TeacherController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
+
     public function index()
     {
-        $exams = Exam::with(['subject.educationLevel', 'subject.educationSystem'])
-        ->select('id', 'subject_id', 'name',)
-        ->get();
 
+        $user = auth()->user();
+        $teacher = Teacher::where('user_id', $user->id)->first();
+        $subject_count = Subject::where('teacher_id', $teacher->id)->count();
+        $exam_count = Exam::where('teacher_id', $teacher->id)->count();
+        $education_systems = EducationSystem::all();
 
-        return view('teachers.dashboard', compact('exams'));
+        $exams = $teacher->exams()
+            ->with(['subject.educationLevel', 'subject.educationSystem'])
+            ->withCount('questions')
+            ->get();
+
+        $topics_subtopics_counts = [];
+        foreach ($exams as $exam) {
+            $topicStrands = $exam->questions()->distinct('topic_strand_id')->count('topic_strand_id');
+            $subTopicStrands = $exam->questions()->distinct('sub_topic_sub_strand_id')->count('sub_topic_sub_strand_id');
+            $topics_subtopics_counts[$exam->id] = ['topicStrands' => $topicStrands, 'subTopicStrands' => $subTopicStrands];
+        }
+
+    return view('teachers.dashboard', compact('education_systems', 'exams', 'topics_subtopics_counts', 'subject_count', 'exam_count'));
     }
 
 
