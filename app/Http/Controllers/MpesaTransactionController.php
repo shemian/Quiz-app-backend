aa0a21ceb095725eeeb57f7a0952ec769c693d12
shemian <shemian092@gmail.com>
2023-07-03 00:27:58 +0300

    get all the mpesa transaction

--- app/Http/Controllers/MpesaTransactionController.php
+++ app/Http/Controllers/MpesaTransactionController.php
@@ -161,7 +161,8 @@ class MpesaTransactionController extends Controller
         // update student centy balance
         $student->centy_balance = floatval($student->centy_balance) + $plan->price/2;
         $student->account_status = AccountStatus::ACTIVE;
-        $student->start_date =
+        $student->start_date = Carbon::now();
+        $student->active_subscription=$planName;
         $student->save();
 
         // Responding to the confirmation request
