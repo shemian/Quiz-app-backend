cfae4becab5b0fc2c020714954214265f30e35ea
shemian <shemian092@gmail.com>
2023-07-02 16:30:36 +0300

    add default value

--- database/migrations/2023_05_24_125742_create_guardians_table.php
+++ database/migrations/2023_05_24_125742_create_guardians_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->foreignUuid('user_id')->constrained()->cascadeOnDelete();
             $table->string('phone_number')->nullable();
-            $table->decimal('credit', 8, 2)->nullable();
-            $table->decimal('debit', 8, 2)->nullable();
+            $table->decimal('credit', 8, 2)->nullable()->default(0);
+            $table->decimal('debit', 8, 2)->nullable()->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
