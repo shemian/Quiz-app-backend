0ca21573449483c3da7adef4d2a7da4fc8b9ceb8
shemian <shemian092@gmail.com>
2023-07-03 03:04:05 +0300

    create a cron job to handle deactivation of the account

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,13 @@
 
 namespace App\Console;
 
+use App\Enums\AccountStatus;
+use App\Models\Student;
+use App\Models\SubscriptionPlan;
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
 
 class Kernel extends ConsoleKernel
 {
@@ -12,7 +17,23 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+
+        $students = Student::where('account_status', AccountStatus::ACTIVE)->first();
+        $subscription_plans = SubscriptionPlan::all();
+
+        foreach ($students as $student) {
+            foreach ($subscription_plans as $subscription_plan) {
+                if ($student->active_subscription == $subscription_plan->name) {
+                    if(Carbon::parse( $student->start_date)->addDays($subscription_plan->validity) <= Carbon::now()){
+                        $student->account_status = AccountStatus::SUSPENDED;
+                        $student->active_subscription = null;
+                        $student->save();
+                    }
+                    $student->credit = $student->credit - $subscription_plan->price;
+                    $student->save();
+                }
+            }
+        }
     }
 
     /**
