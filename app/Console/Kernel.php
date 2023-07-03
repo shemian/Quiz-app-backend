ffe79f8ccb6c8002cf5fe45ecbe86beda43e9722
shemian <shemian092@gmail.com>
2023-07-03 04:54:44 +0300

    create a cron job to handle deactivation of the account

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,10 +19,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+
         $schedule->call(function () {
             Log::info('Cron is working fine!');
             (new AccountStatusController)->changeAccountStatus();
-
         })->everyMinute();
 
 
--- app/Http/Controllers/AccountStatusController.php
+++ app/Http/Controllers/AccountStatusController.php
@@ -7,28 +7,33 @@ use App\Models\Student;
 use App\Models\SubscriptionPlan;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class AccountStatusController extends Controller
 {
-
     public function changeAccountStatus()
     {
+        try {
+            $students = Student::where('account_status', AccountStatus::ACTIVE)->get();
+            $subscriptionPlans = SubscriptionPlan::all();
 
-        $students = Student::where('account_status', AccountStatus::ACTIVE)->first();
-        $subscription_plans = SubscriptionPlan::all();
-
-        foreach ($students as $student) {
-            foreach ($subscription_plans as $subscription_plan) {
-                if ($student->active_subscription == $subscription_plan->name) {
-                    if(Carbon::parse( $student->start_date)->addDays($subscription_plan->validity) <= Carbon::now()){
-                        $student->account_status = AccountStatus::SUSPENDED;
-                        $student->active_subscription = null;
+            foreach ($students as $student) {
+                foreach ($subscriptionPlans as $subscriptionPlan) {
+                    if ($student->active_subscription === $subscriptionPlan->name) {
+                        if (Carbon::parse($student->start_date)->addDays($subscriptionPlan->validity)->isPast()) {
+                            $student->account_status = AccountStatus::SUSPENDED;
+                            $student->active_subscription = null;
+                            $student->save();
+                        }
+                        $student->credit -= $subscriptionPlan->price;
                         $student->save();
                     }
-                    $student->credit = $student->credit - $subscription_plan->price;
-                    $student->save();
                 }
             }
+
+            Log::info('Cron job executed successfully!');
+        } catch (\Exception $e) {
+            Log::error('An error occurred in the cron job: ' . $e->getMessage());
         }
     }
 }
