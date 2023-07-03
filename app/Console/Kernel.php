61396955d34f95465b07cd81611ca7c85aa0cb72
shemian <shemian092@gmail.com>
2023-07-03 05:40:00 +0300

    create a cron job to handle deactivation of the account

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -25,6 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('account:status')->everyMinute();
+        Log::info('Cron job executed successfully!');
     }
 
     /**
