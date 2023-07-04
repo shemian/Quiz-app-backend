cff07fae0cc70266a9d83fdf570bb33fd3c35869
shemian <shemian092@gmail.com>
2023-07-04 21:11:09 +0300

    Cron Job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('students:update-accounts')
-            ->everyMinute();
+            ->cron('1 * * * *');
     }
 
 
