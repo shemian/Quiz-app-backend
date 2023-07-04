9121244b8bcfd723be27ef898caa1216fdb40677
shemian <shemian092@gmail.com>
2023-07-05 02:52:06 +0300

    Update student deactivation

--- app/Console/Commands/UpdateStudentsAccountStatus.php
+++ app/Console/Commands/UpdateStudentsAccountStatus.php
@@ -38,8 +38,7 @@ class UpdateStudentsAccountStatus extends Command
                 foreach ($subscriptionPlans as $subscriptionPlan) {
                     if ($student->studentSubscriptionPlan->subscriptionPlan->id === $subscriptionPlan->id) {
                         $end_date = Carbon::parse($student->studentSubscriptionPlan->start_date)->addDays($subscriptionPlan->validity);
-                        Log::info("Plan status is: ". ($end_date >= Carbon::now()));
-                        if ($end_date->lessThan(Carbon::now())) {
+                        if ($end_date <= Carbon::now()) {
                             Log::info("Account should be suspended");
                             $student->account_status = AccountStatus::SUSPENDED;
                             $student->active_subscription = null;
