6b73759ff7c2334c3c158f4bb66bde894e0c859f
shemian <shemian092@gmail.com>
2023-07-05 06:29:33 +0300

    Fix sms send

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -21,8 +21,9 @@ class StudentController extends Controller
     public function index()
     {
         $user = Auth::user();
-        $student = Student::where('user_id', $user->id)->first();
+        if(!isset($user)) return redirect()->route('login')->with("error", "Please login to continue");
 
+        $student = Student::where('user_id', $user->id)->first();
         Log::info('Student: ' . $student);
 
         // Get the result of the student and display it on the dashboard because results has the exam_id and the student_id and exams has question so will dipslay the number of exams, questions in that exam and date of that result
