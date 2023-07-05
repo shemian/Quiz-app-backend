d498642406a9cf7fb4559ac24c617df798f5cbab
shemian <shemian092@gmail.com>
2023-07-05 15:27:08 +0300

    get random questions

---
+++ resources/views/students/brain_game.blade.php
@@ -0,0 +1,7 @@
+@extends('students.master')
+
+@section('content')
+    <h1>'Brain Game'</h1>
+
+
+@endsection
--- resources/views/students/inc/left-nav.blade.php
+++ resources/views/students/inc/left-nav.blade.php
@@ -43,7 +43,7 @@
             </li>
 
             <li class="side-nav-item">
-                <a href="#" class="side-nav-link">
+                <a href="{{ route('random_game') }}" class="side-nav-link">
                     <i class="uil-game-structure"></i>
                     <span> Brain Game </span>
                 </a>
