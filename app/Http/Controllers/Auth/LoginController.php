1906e76d6aaddf2f1dd4bddf19bde8dfae4d7af8
Brackie <muimikieleko@gmail.com>
2023-07-13 23:24:59 +0300

    fixed merge conflict

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,7 +6,7 @@ use App\Jobs\SendOtpOnLogin;
 use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
- use App\Http\Controllers\Controller;
+use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Hash;
