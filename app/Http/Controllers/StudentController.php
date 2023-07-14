5577b2566acc03ecce34675676fc09e1e893270b
shemian <shemian092@gmail.com>
2023-07-14 17:14:38 +0300

    implemented brain game

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -264,7 +264,7 @@ class StudentController extends Controller
         // Retrieve questions
         $questions = Question::where('education_level_id', $student->educationLevel->id)
             ->inRandomOrder()
-            ->take(3) // Change the number to the desired amount of questions
+            ->take(10) // Change the number to the desired amount of questions
             ->get();
 
 
