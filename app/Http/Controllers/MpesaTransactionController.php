6fcec090a462f79c75eb1f343a848b5681736ec7
shemian <shemian092@gmail.com>
2023-07-14 03:45:23 +0300

    set up student controller to display the questions

--- app/Http/Controllers/MpesaTransactionController.php
+++ app/Http/Controllers/MpesaTransactionController.php
@@ -93,6 +93,7 @@ class MpesaTransactionController extends Controller
         $response->setContent($result);
         return $response;
     }
+
     /**
      *  M-pesa Validation Method
      * Safaricom will only call your validation if you have requested by writing an official letter to them
@@ -103,8 +104,9 @@ class MpesaTransactionController extends Controller
         $result_description = "Accepted validation request.";
         return $this->createValidationResponse($result_code, $result_description);
     }
+
     /**
-     * M-pesa Transaction confirmation method, we save the transaction in our databases
+     * M-pesa Transaction confirmation method for C2B, we save the transaction in our databases
      */
     public function mpesaConfirmation(Request $request)
     {
@@ -183,7 +185,139 @@ class MpesaTransactionController extends Controller
         return $response;
     }
 
+    /**
+     * M-pesa B2C API
+     */
 
+    public function b2CTransaction($phone_number, $amount, $centyPlusId, $planName)
+    {
+        $url ='https://api.safaricom.co.ke/mpesa/b2c/v1/paymentrequest';
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$this->generateAccessToken()));
+
+        $formattedAmount = number_format($amount, 0, '', '');
+//        $formattedPhoneNumber = '254' . substr($phone_number, 1);
+        $formattedPhoneNumber = GeneralHelper::phoneNumberToInternational($phone_number);
+        Log::info($formattedPhoneNumber);
+
+        $curl_post_data = [
+            //Fill in the request parameters with valid values
+            "InitiatorName" => "testapiuser",
+            "SecurityCredential" => "***********",
+            "Occassion" => "StallOwner",
+            "CommandID" => "BusinessPayment",
+            "PartyA" => "600000",
+            "PartyB" => "254711959143",
+            "Remarks" => "Test B2C",
+            "Amount" => 100,
+            "QueueTimeOutURL" => "http://myservice:8080/b2c/result",
+            "ResultURL" => "http://myservice:8080/b2c/result"
+        ];
+        $data_string = json_encode($curl_post_data);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+        $curl_response = curl_exec($curl);
+        Log::info("Callback Confirmation: ". $curl_response);
+
+        return $curl_response;
+    }
+
+    /**
+     *  M-pesa Validation Method
+     * Safaricom will only call your validation if you have requested by writing an official letter to them
+     */
+    public function mpesaB2CValidation(Request $request)
+    {
+        $result_code = "0";
+        $result_description = "Accepted validation request.";
+        return $this->createValidationResponse($result_code, $result_description);
+    }
+
+    /**
+     * M-pesa Transaction confirmation method for B2C, we save the transaction in our databases
+     */
+    public function mpesaB2CConfirmation(Request $request)
+    {
+        Log::info('Mpesa'.$request);
+
+        $content=json_decode($request->getContent());
+        $mpesa_transaction = new MpesaTransaction();
+        $mpesa_transaction->transaction_type = $content->TransactionType;
+        $mpesa_transaction->trans_id = $content->TransID;
+        $mpesa_transaction->trans_time = $content->TransTime;
+        $mpesa_transaction->trans_amount = $content->TransAmount;
+        $mpesa_transaction->business_short_code = $content->BusinessShortCode;
+        $mpesa_transaction->bill_ref_number = $content->BillRefNumber;
+        $mpesa_transaction->invoice_number = $content->InvoiceNumber;
+        $mpesa_transaction->org_account_balance = $content->OrgAccountBalance;
+        $mpesa_transaction->third_party_trans_id = $content->ThirdPartyTransID;
+        $mpesa_transaction->msisdn = $content->MSISDN;
+        $mpesa_transaction->first_name = $content->FirstName;
+        if (isset($content->MiddleName)) $mpesa_transaction->middle_name = $content->MiddleName;
+        if (isset($content->LastName)) $mpesa_transaction->last_name = $content->LastName;
+        $mpesa_transaction->save();
+
+        $accRef = explode(' ', $content->BillRefNumber);
+        $planName = $accRef[1];
+        $centyPlusId = $accRef[0];
+
+        $user = User::where('centy_plus_id', $centyPlusId)->first();
+        $student = Student::where('user_id', $user->id)->first();
+        $plan = SubscriptionPlan::where('name', $planName)->first();
+
+        // check if transaction amount is insufficient
+        if ($content->TransAmount < $plan->price) {
+            // add surplus to the parent account
+            $student->guardian->credit = floatval($student->guardian->credit) + $content->TransAmount;
+            $student->guardian->save();
+
+            $response = new Response();
+            $response->headers->set("Content-Type","text/xml; charset=utf-8");
+            $response->setContent(json_encode([
+                "C2BPaymentConfirmationResult"=>"Insufficient Amount"
+            ]));
+            return $response;
+        }
+
+        $chart_of_account = ChartOfAccounts::where('account_name', 'Business Account')->first();
+        $chart_of_account->account_balance = $chart_of_account->account_balance + $plan->price/2;
+        $chart_of_account->save();
+
+        // add surplus to the parent account
+        $student->guardian->credit = floatval($student->guardian->credit) + ($content->TransAmount - $plan->price);
+        $student->guardian->save();
+
+        // Create or Update student subscription plan
+        $start_date  = Carbon::now();
+        $end_date = Carbon::now()->addDays($plan->validity);
+        $studentSubscription = StudentSubscriptionPlan::updateOrCreate(
+            ['student_id' => $student->id],
+            [
+                'subscription_plan_id' => $plan->id,
+                'start_date' => $start_date,
+                'end_date' => $end_date,
+            ]
+        );
+
+        // update student centy balance
+        $student->centy_balance = floatval($student->centy_balance) + $plan->price/2;
+        $student->account_status = AccountStatus::ACTIVE;
+        $student->active_subscription = $studentSubscription->id;
+        $student->save();
+
+        // Responding to the confirmation request
+        $response = new Response();
+        $response->headers->set("Content-Type","text/xml; charset=utf-8");
+        $response->setContent(json_encode(["C2BPaymentConfirmationResult"=>"Success"]));
+
+        return $response;
+    }
+
+    /**
+     * M-pesa Register URL Callbacks
+     */
     public function mpesaRegisterUrls()
     {
         $curl = curl_init();
@@ -200,7 +334,5 @@ class MpesaTransactionController extends Controller
         $curl_response = curl_exec($curl);
         echo $curl_response;
     }
-
-
 
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\ExamController;
 use App\Http\Controllers\SubscriptionPlanController;
 use App\Http\Controllers\SubTopicSubStrandController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TeacherController;
 use App\Http\Controllers\GuardianController;
@@ -34,9 +35,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/password/reset', [LoginController::class, 'showPasswordResetForm'])->name('password.reset');
+Route::get('/password/reset', 'App\Http\Controllers\Auth\LoginController@showPasswordResetForm')->name('password.reset');
 Route::post('/password/reset', [LoginController::class, 'resetPassword'])->name('password.update');
-
 
 Route::prefix('/admin')->middleware(['isAdmin'])->group(function(){
     Route::get('/', [AdminController::class, 'index'])->name('admin.dashboard');
@@ -77,6 +77,7 @@ Route::prefix('parent')->middleware(['isParent'])->group(function(){
     Route::post('/edit-students/{id}', [GuardianController::class, 'update'])->name('update_student');
     Route::delete('/delete-students/{id}', [GuardianController::class, 'destroy'])->name('delete_student');
 
+    Route::post('/stk/push', [GuardianController::class, 'activateStudent'])->name('stk_push');
 });
 
 
@@ -119,8 +120,9 @@ Route::prefix('student')->middleware(['auth', 'isStudent'])->group(function(){
     Route::get('/questions/{exam}', [StudentController::class, 'showQuestions'])->name('show_questions');
     Route::post('/questions/{exam}', [StudentController::class, 'submitAnswers'])->name('questions.submit');
     Route::get('/view_result/{result}', [StudentController::class, 'viewResult'])->name('students.view_results');
+    Route::post('/withdraw', [StudentController::class, 'studentWithdrawal'])->name('student_withdrawal');
 
-
+    Route::post('/save-result', [StudentController::class, 'saveExamResult'])->name('save_result');
 });
 
 
