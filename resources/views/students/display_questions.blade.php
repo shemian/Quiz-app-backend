b5c565d21d61c6209373c128a3f6b012fd2f422a
shemian <shemian092@gmail.com>
2023-07-14 03:02:29 +0300

    set up student controller to display the questions

--- resources/views/students/display_questions.blade.php
+++ resources/views/students/display_questions.blade.php
@@ -69,7 +69,7 @@
                 <!-- Here I've inserted Score Result from JavaScript -->
             </div>
             <div class="buttons">
-                <button class="quit">Exit </button>
+                <button class="quit">Quit Quiz</button>
             </div>
         </div>
 
@@ -121,27 +121,8 @@
         let counterLine;
         let widthValue = 0;
 
-        const restart_quiz = result_box.querySelector(".buttons .restart");
         const quit_quiz = result_box.querySelector(".buttons .quit");
 
-        // if restartQuiz button clicked
-        restart_quiz.onclick = ()=>{
-            quiz_box.classList.add("activeQuiz"); //show quiz box
-            result_box.classList.remove("activeResult"); //hide result box
-            timeValue = 15;
-            que_count = 0;
-            que_numb = 1;
-            userScore = 0;
-            widthValue = 0;
-            showQuetions(que_count); //calling showQestions function
-            queCounter(que_numb); //passing que_numb value to queCounter
-            clearInterval(counter); //clear counter
-            clearInterval(counterLine); //clear counterLine
-            startTimer(timeValue); //calling startTimer function
-            startTimerLine(widthValue); //calling startTimerLine function
-            timeText.textContent = "Time Left"; //change the text of timeText to Time Left
-            next_btn.classList.remove("show"); //hide the next button
-        }
 
         // if quitQuiz button clicked
         quit_quiz.onclick = ()=>{
