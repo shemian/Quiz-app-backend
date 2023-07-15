59ff83a7b9541edb055bec9e821de4331030aa06
shemian <shemian092@gmail.com>
2023-07-15 11:47:15 +0300

    implemented brain game

--- resources/views/students/view_result.blade.php
+++ resources/views/students/view_result.blade.php
@@ -1,14 +1,12 @@
 @extends('students.master')
 
 @section('content')
-    <div class="container">
+    <div class="container text-center">
         <h4>View Results</h4>
         <h4>Exam: {{ $exam->name }}</h4>
 
         <div class="card mx-auto" style="width: 18rem;">
             <div class="card-body">
-                <h3 class="card-title">Correctly Answered Questions</h3>
-                <p class="card-text">{{ $result->yes_ans }}</p>
                 <h4 class="card-title">Congratulations! 🎉</h4>
                 <p class="card-text">You got <strong>{{ $result->yes_ans }}</strong> out of <strong>{{ $result->yes_ans + $result->no_ans }}</strong></p>
             </div>
