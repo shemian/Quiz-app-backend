7a5c318ba3738390a9095de45800021afb3144da
shemian <shemian092@gmail.com>
2023-07-15 11:38:03 +0300

    implemented brain game

--- resources/views/students/view_result.blade.php
+++ resources/views/students/view_result.blade.php
@@ -8,22 +8,9 @@
 
         <h3>Correctly Answered Questions</h3>
         <p>{{ $result->yes_ans }}</p>
+        <span>Congratulations! 🎉 You got <p>{{ $result->yes_ans }}</p> out of <p>{{ $result->yes_ans + $result->no_ans }}</p></span>
 
         <h3>Incorrectly Answered Questions</h3>
         <p>{{ $result->no_ans }}</p>
-
-        @if ($result->no_ans > 0)
-            <h4>Incorrect Answer Details</h4>
-            @foreach (json_decode($result->result_json, true)['answers'] as $answer)
-                @if (isset($answer['correct_answer']) && $answer['answer'] !== $answer['correct_answer'])
-                    <p>Question: {{ $answer['question'] }}</p>
-                    <p>Correct Answer: {{ $answer['correct_answer'] }}</p>
-                    <p>Your Answer: {{ $answer['answer'] }}</p>
-                @endif
-            @endforeach
-        @else
-            <p>No questions answered incorrectly.</p>
-        @endif
     </div>
-
 @endsection
