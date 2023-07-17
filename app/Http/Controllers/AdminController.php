e71ddc807321005331d2011924efd5fdb4a2d51b
shemian <shemian092@gmail.com>
2023-07-17 19:47:15 +0300

    Display the students details

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -29,6 +29,7 @@ class AdminController extends Controller
         $studentCount = Student::count();
         $teacherCount = User::where('role', 'teacher')->count();
         $organization_revenue = ChartOfAccounts::all();
+        dd($organization_revenue);
         return view ('admin.dashboard', compact('customerCount','studentCount','teacherCount', 'organization_revenue'));
     }
 
