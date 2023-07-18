788fb4d16ff0accc7df84921cd5578e9726f0a02
shemian <shemian092@gmail.com>
2023-07-18 22:27:29 +0300

    view parent details

--- resources/views/auth/otp.blade.php
+++ resources/views/auth/otp.blade.php
@@ -11,66 +11,34 @@
                         <div class="card-body p-4">
 
                             <div class="text-center w-75 m-auto">
-                                <h4 class="text-dark-50 text-center pb-0 fw-bold">Sign In</h4>
-                                <p class="text-muted mb-4">Enter your Centy Plus ID or Email to Login</p>
+                                <h4 class="text-dark-50 text-center pb-0 fw-bold">Verify OTP</h4>
+                                <p class="text-muted mb-4">Enter OTP received on you registered phone number</p>
                             </div>
 
-                            <form method="POST" action="{{ route('login') }}">
+                            <form method="POST" action="{{ route('otp.validate') }}">
                                 @csrf
 
                                 <div class="mb-3">
-                                    <label for="centy_plus_id" class="form-label">Centy Plus ID or Email</label>
+                                    <label for="centy_plus_otp" class="form-label">Centy OTP</label>
                                     <div class="input-group">
                                         <span class="input-group-text">CNT-</span>
-                                        <input id="centy_plus_id" type="text" class="form-control @error('centy_plus_id') is-invalid @enderror" name="centy_plus_id" value="{{ old('centy_plus_id') }}" required autocomplete="centy_plus_id" autofocus>
-                                        @error('centy_plus_id')
+                                        <input id="centy_plus_otp" type="text" class="form-control @error('centy_plus_otp') is-invalid @enderror" name="centy_plus_otp" value="{{ old('centy_plus_otp') }}" required autocomplete="centy_plus_otp" autofocus>
+                                        @isset($error)
                                         <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
+                                            <strong>{{ $error }}</strong>
                                         </span>
                                         @enderror
-                                    </div>
-                                </div>
-
-                                <div class="mb-3">
-                                    @if (Route::has('password.request'))
-                                        <a href="{{ route('password.request') }}" class="text-muted float-end"><small>Forgot your password?</small></a>
-                                    @endif
-                                    <label for="password" class="form-label">Pin</label>
-                                    <div class="input-group input-group-merge">
-                                        <input id="password" type="password" pattern="\d*" inputmode="numeric" minlength="4" maxlength="4" placeholder="Enter 4-digit PIN" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                                        @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                        @enderror
-                                        <div class="input-group-text" data-password="false">
-                                            <span class="password-eye"></span>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="mb-3 mb-3">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }} checked>
-                                        <label class="form-check-label" for="checkbox-signin">Remember me</label>
                                     </div>
                                 </div>
 
                                 <div class="mb-3 mb-0 text-center">
-                                    <button class="btn btn-primary" type="submit"> Log In </button>
+                                    <button class="btn btn-primary" type="submit">Validate</button>
                                 </div>
 
                             </form>
                         </div> <!-- end card-body -->
                     </div>
                     <!-- end card -->
-
-                    <div class="row mt-3">
-                        <div class="col-12 text-center">
-                            <p class="text-muted">Don't have an account? <a href="{{ route('register') }}" class="text-muted ms-1"><b>Sign Up</b></a></p>
-                        </div> <!-- end col -->
-                    </div>
-                    <!-- end row -->
 
                 </div> <!-- end col -->
             </div>
