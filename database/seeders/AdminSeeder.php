ede678ea193decf72392075fff2a2ccb623e6f52
Brackie <muimikieleko@gmail.com>
2023-07-22 14:58:50 +0300

    [chore:] change seeder number

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -18,24 +18,24 @@ class AdminSeeder extends Seeder
             'name' => 'admin',
             'email' => 'admin@admin.com',
             'password' => bcrypt('0000'),
-            'phone_number' => '0798898678',
+            'phone_number' => '0711637755',
             'role' => 'admin',
             'first_login' => false
         ]);
 
-       $user = User::create([
-            'name' => 'Teacher admin',
-            'email' => 'teacher@admin.com',
-            'password' => bcrypt('0011'),
-            'phone_number' => '0798898678',
-            'role' => 'teacher',
-            'first_login' => false
-        ]);
+    //    $user = User::create([
+    //         'name' => 'Teacher admin',
+    //         'email' => 'teacher@admin.com',
+    //         'password' => bcrypt('0011'),
+    //         'phone_number' => '0711637755',
+    //         'role' => 'teacher',
+    //         'first_login' => false
+    //     ]);
 
-        Teacher::create([
-            'user_id' => $user->id,
-            'phone_number' => '0798898678',
-        ]);
+    //     Teacher::create([
+    //         'user_id' => $user->id,
+    //         'phone_number' => '0711637755',
+    //     ]);
 
     }
 }
