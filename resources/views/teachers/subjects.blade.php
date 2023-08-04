220c75e1187ee26bca36929c1b6c89db49a9c6be
shemian <shemian092@gmail.com>
2023-08-04 15:10:35 +0300

    Update the question blade code and remove the edit and delete

--- resources/views/teachers/subjects.blade.php
+++ resources/views/teachers/subjects.blade.php
@@ -40,7 +40,6 @@
 
     <!-- Scrollable modal -->
 
-
     <!-- Add Questions Modal -->
     <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
